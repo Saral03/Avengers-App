e3c20fc8b1094a61ae1e68e234e0a07213ba30cf
Saral03 <saralhanda13@gmail.com>
2022-06-30 00:39:37 +0530

    Validated user input

--- app/src/main/java/com/example/avengers/New_Activity.kt
+++ app/src/main/java/com/example/avengers/New_Activity.kt
@@ -6,26 +6,35 @@ import android.os.Bundle
 import android.widget.*
 
 lateinit var logo:ImageView
-    lateinit var email:EditText
+    lateinit var mobile:EditText
     lateinit var pass:EditText
     lateinit var fp:TextView
     lateinit var register:TextView
     lateinit var but:Button
+    val mob="7905796404"
+    val pwd="Spicemi5"
 class New_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
         title="Log In"
         logo=findViewById(R.id.logo)
-        email=findViewById(R.id.email)
+        mobile=findViewById(R.id.mobile)
         pass=findViewById(R.id.pass)
         fp=findViewById(R.id.fp)
         register=findViewById(R.id.register)
         but=findViewById(R.id.but)
         but.setOnClickListener {
-            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-            val intent=Intent(this@New_Activity,MainActivity::class.java)
-            startActivity(intent)
+            val check_mob= mobile.text.toString()
+            val check_password=pass.text.toString()
+            if (mob==check_mob && pwd==check_password){
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                val intent=Intent(this@New_Activity,MainActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, "Invalid Mobile/Password", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
